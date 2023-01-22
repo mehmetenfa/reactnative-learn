973fd6f7a464b0a479651f2a312fad720eb1e636
qxan <mehmet.enfa@gmail.com>
2023-01-22 21:17:19 +0300

    try react native style

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'lime', flexDirection: 'row' }}>
+      <View style={{ backgroundColor: "lime", flex: 1, alignSelf: 'flex-start' }}>
+        <Text>react native öğreniyoz hocam</Text>
+      </View>
+      <View style={{ backgroundColor: 'rebeccapurple', flex: 1, alignSelf: 'flex-start'  }}>
+        <Text>react native öğreniyoruz</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
